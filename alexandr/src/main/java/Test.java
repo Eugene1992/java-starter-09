4a7e6c63138a7ef6ce8fdc48a1ea20419b84f13c
Eugene1992 <deyneko55@gmail.com>
2016-07-19 09:10:12 +0300

    Initial commit

---
+++ alexandr/src/main/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Student on 7/19/2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/examples/CW01.java
@@ -0,0 +1,6 @@
+
+public class CW01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
